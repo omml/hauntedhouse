c723fa8ee7c9ebfa83eea61b23bbac9a3dd00598
omml <omml@hotmail.com>
2024-07-09 22:30:07 +0100

    Update main.cpp
    Improved HandleAction function

--- main.cpp
+++ main.cpp
@@ -42,6 +42,204 @@ bool IsMovingAction(Action* a)
     return retVal;
 }
 
+bool HandlePickAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Check if we are holding an item
+    if (item->getNumHoldingItems() == 0)
+    {
+        // Add the item to hand inventory
+        if (hand->addItem(item, m))
+        {
+            // Set time as picked
+            item->picked();
+            // Remove item from room if in room
+            r->removeItem(item);
+            // Remove item from bag if in bag
+            bag->removeItem(item);
+            // Remove item from wearing if wearing
+            body->removeItem(item);
+            *m = "The " + item->getName() + " was picked and now you are holding it";
+            retVal = true;
+        }
+        else
+        {
+            retVal = false;
+        }
+    }
+    else
+    {
+        *m = "The " + item->getName() + " can not be picked while it is hokding items";
+        retVal = false;
+    }
+
+    return retVal;
+}
+
+bool HandleStoreAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Add item to bag inventory
+    if (bag->addItem(item, m))
+    {
+        // Check if is an item that can be lighted
+        if (item->isPickStoreLight())
+        {
+            // Check if it is lighted
+            if (item->getState() == Lighted)
+            {
+                // Unlight it
+                r->setIsLighted(false, false);
+            }
+        }
+        // Set item as stored
+        item->stored();
+        // Remove it from room 
+        r->removeItem(item);
+        // Remove it from hand if in hand
+        hand->removeItem(item);
+        // Remove it from body if in body
+        body->removeItem(item);
+        *m = "The " + item->getName() + " was stored in your bag";
+        retVal = true;
+    }
+    else
+    {
+        retVal = false;
+    }
+
+    return retVal;
+}
+
+bool HandleDropAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Set item as dropped
+    item->dropped();
+    // Add it to the room
+    r->setItem(item);
+    // Set if room has a candle
+    r->hasDroppedCandle(item);
+    item->setPosDesc("Laying on the floor");
+
+    // Remove it from hand if in hand
+    hand->removeItem(item);
+    // Remove it from bag if in bag
+    bag->removeItem(item);
+    // Remove from body if in body
+    body->removeItem(item);
+    *m = "The " + item->getName() + " has been dropped to the floor";
+    retVal = true;
+
+    return retVal;
+}
+
+bool HandleWearAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Add item to body inventory
+    if (body->addItem(item, m))
+    {
+        // Set item as wore
+        item->wore();
+        // Remove from roon
+        r->removeItem(item);
+        // Remove from hand if in hand
+        hand->removeItem(item);
+        // Remove from bag if in bag
+        bag->removeItem(item);
+        *m = "You are wearing the " + item->getName();
+
+        retVal = true;
+    }
+    else
+    {
+        retVal = false;
+    }
+    return retVal;
+}
+
+bool HandleMoveAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Move to a location in index moveIdx
+    item->moved(moveIdx);
+
+    *m = "The " + item->getName() + " has been moved";
+    retVal = true;
+
+    return retVal;
+}
+
+bool HandleLightAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Check if we are holding matches
+    if (hand->areThereMatches() == true)
+    {
+        // Check if we have a free hand to light the item
+        if (hand->getNumItems() == 1)
+        {
+            retVal = true;
+            *m = "You have lighted the " + item->getName();
+            item->lighted();
+            r->setIsLighted(true, false);
+        }
+        else
+        {
+            retVal = false;
+            *m = "You need the matches in one hand and the other hand free to light the " + item->getName();
+        }
+    }
+    else
+    {
+        // Matches are need to light
+        if (bag->areThereMatches() == true)
+        {
+            retVal = false;
+            *m = "You need to hold the matches";
+        }
+        else
+        {
+            retVal = false;
+            *m = "You need matches";
+        }
+    }
+
+    return retVal;
+}
+
+bool HandleReadAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Read item
+    *m = item->read();
+    retVal = true;
+
+    return retVal;
+}
+
+bool HandleClimbAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
+{
+    bool retVal = false;
+
+    // Climb item
+    GameState::getInstance().setClimbedItem(item);
+
+    *m = "You climbed the " + item->getName();
+
+    retVal = true;
+
+    return retVal;
+}
+
 bool HandleAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* bag, Inventory* body, string* m, int moveIdx)
 {
     bool retVal = false;
@@ -49,163 +247,36 @@ bool HandleAction(Room* r, Action* a, Item* item, Inventory* hand, Inventory* ba
     switch(a->GetActionID())
     {
     case AIDPick:
-        // Check if we are holding an item
-        if (item->getNumHoldingItems() == 0)
-        {
-            // Add the item to hand inventory
-            if (hand->addItem(item, m))
-            {
-                // Set time as picked
-                item->picked();
-                // Remove item from room if in room
-                r->removeItem(item);
-                // Remove item from bag if in bag
-                bag->removeItem(item);
-                // Remove item from wearing if wearing
-                body->removeItem(item);
-                *m = "The " + item->getName() + " was picked and now you are holding it";
-                retVal = true;
-            }
-            else
-            {
-                retVal = false;
-            }
-        }
-        else
-        {
-            *m = "The " + item->getName() + " can not be picked while it is hokding items";
-            retVal = false;
-        }
+        retVal = HandlePickAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDStore:
-        // Add item to bag inventory
-        if (bag->addItem(item, m))
-        {
-            // Check if is an item that can be lighted
-            if (item->isPickStoreLight())
-            {
-                // Check if it is lighted
-                if (item->getState() == Lighted)
-                {
-                    // Unlight it
-                    r->setIsLighted(false, false);
-                }
-            }
-            // Set item as stored
-            item->stored();
-            // Remove it from room 
-            r->removeItem(item);
-            // Remove it from hand if in hand
-            hand->removeItem(item);
-            // Remove it from body if in body
-            body->removeItem(item);
-            *m = "The " + item->getName() + " was stored in your bag";
-            retVal = true;
-        }
-        else
-        {
-            retVal = false;
-        }
+        retVal = HandleStoreAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDDrop:
-        // Set item as dropped
-        item->dropped();
-        // Add it to the room
-        r->setItem(item);
-        // Set if room has a candle
-        r->hasDroppedCandle(item);
-        item->setPosDesc("Laying on the floor");
-        
-        // Remove it from hand if in hand
-        hand->removeItem(item);
-        // Remove it from bag if in bag
-        bag->removeItem(item);
-        // Remove from body if in body
-        body->removeItem(item);
-        *m = "The " + item->getName() + " has been dropped to the floor";
-        retVal = true;
+        retVal = HandleDropAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDWear:
-        // Add item to body inventory
-        if (body->addItem(item, m))
-        {
-            // Set item as wore
-            item->wore();
-            // Remove from roon
-            r->removeItem(item);
-            // Remove from hand if in hand
-            hand->removeItem(item);
-            // Remove from bag if in bag
-            bag->removeItem(item);
-            *m = "You are wearing the " + item->getName();
-            
-            retVal = true;
-        }
-        else
-        {
-            retVal = false;
-        }
+        retVal = HandleWearAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDMove:
-        // Move to a location in index moveIdx
-        item->moved(moveIdx);
-
-        *m = "The " + item->getName() + " has been moved";
-        retVal = true;
+        retVal = HandleMoveAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDLight:
-        // Check if we are holding matches
-        if (hand->areThereMatches() == true)
-        {
-            // Check if we have a free hand to light the item
-            if (hand->getNumItems() == 1)
-            {
-                retVal = true;
-                *m = "You have lighted the " + item->getName();
-                item->lighted();
-                r->setIsLighted(true, false);
-            }
-            else
-            {
-                retVal = false;
-                *m = "You need the matches in one hand and the other hand free to light the " + item->getName();
-            }
-        }
-        else
-        {
-            // Matches are need to light
-            if (bag->areThereMatches() == true)
-            {
-                retVal = false;
-                *m = "You need to hold the matches";
-            }
-            else
-            {
-                retVal = false;
-                *m = "You need matches";
-            }
-        }
+        retVal = HandleLightAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case  AIDRead:
-        // Read item
-        *m = item->read();
-        retVal = true;
+        retVal = HandleReadAction(r, a, item, hand, bag, body, m, moveIdx);
         break;
 
     case AIDClimb:
-
-        // Climb item
-        GameState::getInstance().setClimbedItem(item);
-
-        *m = "You climbed the " + item->getName();
-
-        retVal = true;
+        retVal = HandleClimbAction(r, a, item, hand, bag, body, m, moveIdx);
+        
         break;
     default:
         break;
@@ -630,3 +701,4 @@ int main()
 
     } while (mainOption != 0);
 }
+
